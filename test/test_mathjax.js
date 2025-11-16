3fd94514ac05d860b453c0aa1486e16b887f9edc
Douglas Hanley <thesecretaryofwar@gmail.com>
2025-11-16 11:38:33 -0600

    update node mathjax base example

--- test/test_mathjax.js
+++ test/test_mathjax.js
@@ -3,9 +3,6 @@ import { source } from '@mathjax/src/components/js/source.js';
 // configure MathJax
 global.MathJax = {
   loader: {
-    paths: {
-        mathjax: '@mathjax/src/bundle'
-    },
     load: [ 'adaptors/liteDOM', 'input/tex', 'output/svg' ],
     require: (file => import(file)),
     source,
@@ -20,10 +17,12 @@ global.MathJax = {
 await import(source['tex-mml-svg'])
 await MathJax.startup.promise
 
-// your code that uses MathJax here
+// render to litedom
 const mml = MathJax.tex2mml('x^2')
 const dom = MathJax.mathml2svg(mml)
 const tre = dom.children[0]
+
+// serialize to svg
 const svg = MathJax.startup.adaptor.serializeXML(tre)
 console.log(svg)
 
