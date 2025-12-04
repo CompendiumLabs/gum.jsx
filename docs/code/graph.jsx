31eda6e647846d62b8ed509ca6ab90348eed2f2e
Douglas Hanley <thesecretaryofwar@gmail.com>
2025-12-04 00:39:19 -0600

    improve docs

--- docs/code/graph.jsx
+++ docs/code/graph.jsx
@@ -1,6 +1,6 @@
 // a series of closely spaced squares rotating clockwise along a sinusoidal path
 <Graph padding={[0.2, 0.6]}>
   <DataPoints fy={sin} xlim={[0, 2*pi]} size={0.5} N={150}>
-    { x => <Square spin={r2d*x} /> }
+    { x => <Square rounded spin={r2d*x} /> }
   </DataPoints>
 </Graph>
