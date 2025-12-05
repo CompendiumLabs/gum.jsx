1c5c4eaa932b28e59fe6842fe32cab707c15689a
Douglas Hanley <thesecretaryofwar@gmail.com>
2025-12-05 13:58:19 -0600

    remove emoji docs

--- docs/code/emoji.jsx
+++
@@ -1,8 +0,0 @@
-// draw a row of emojis with various faces
-<HStack spacing={0.1}>
-  { [ 'grinning', 'neutral_face', 'scream', 'joy', 'heart_eyes' ].map(t =>
-    <Frame rounded fill>
-      <Emoji>{t}</Emoji>
-    </Frame>
-  ) }
-</HStack>
