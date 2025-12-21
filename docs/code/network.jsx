2bd218664165f16f9aa986f443324f1cae311992
Douglas Hanley <thesecretaryofwar@gmail.com>
2025-12-21 11:24:45 -0600

    docs typo in network

--- docs/code/network.jsx
+++ docs/code/network.jsx
@@ -1,5 +1,5 @@
 // A network with a node on the left saying "hello world" and two nodes on the right saying "hello" and "world". There are arrows going from the left node to each of the right nodes. The nodes have gray backgrounds and rounded corners.
-<Network coord={[0, 0, 2, 1]} node-rad={[0.2, 0.1]} node-wrap={4} node-fill={gray} dge-arrow-size={0.07} aspect={phi}>
+<Network coord={[0, 0, 2, 1]} node-rad={[0.2, 0.1]} node-wrap={4} node-fill={gray} edge-arrow-size={0.07} aspect={phi}>
   <Node label="hello world" pos={[0.4, 0.5]} rad={[0.25, 0.15]}>Hello world</Node>
   <Node label="hello" pos={[1.4, 0.25]}>Hello</Node>
   <Node label="world" pos={[1.6, 0.75]}>World!</Node>
