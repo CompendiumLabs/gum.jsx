74dc1a70ef8f7fd0b75c5eece3eb8f73ffb609b9
Douglas Hanley <thesecretaryofwar@gmail.com>
2026-01-09 22:36:43 -0600

    update gallery

--- docs/gallery/flux_capacitance.jsx
+++ docs/gallery/flux_capacitance.jsx
@@ -1,5 +1,5 @@
 <Plot grid margin={0.3} xlim={[0, 2*pi]} ylim={[-1.5, 1.5]} xlabel="Phase (radians)" ylabel="Interference" title="Flux Capacitance">
   <SymFill fy1={sin} fy2={cos} fill={blue} opacity={0.25} />
-  <SymLine fy={sin} />
-  <SymLine fy={cos} />
+  <SymCurve fy={sin} />
+  <SymCurve fy={cos} />
 </Plot>
---
+++ docs/gallery/spline_star.jsx
@@ -0,0 +1,20 @@
+// parameters
+const n = 5
+const R = 0.7
+const c = 0.6
+
+// get inner/outer vertex angles
+const theta0 = linspace(0, 2 * pi, n + 1).slice(0, -1).map(t => t - pi / 2)
+const theta1 = theta0.map(t => t + pi / n)
+
+// get inner/outer point positions
+const polar = (r, t) => [ r * cos(t), r * sin(t) ]
+const points0 = theta0.map(t => polar(1, t))
+const points1 = theta1.map(t => polar(R, t))
+
+// return full spline
+return <Frame margin padding rounded fill={gray}>
+  <Spline closed fill={blue} curve={c} coord={[-1, -1, 1, 1]}>
+    {zip(points0, points1).flat()}
+  </Spline>
+</Frame>
