f14007ba228bd9366420de4698fe3de4a209f431
Douglas Hanley <thesecretaryofwar@gmail.com>
2026-01-14 13:47:41 -0600

    fix snake wave

--- docs/code/graph.jsx
+++ docs/code/graph.jsx
@@ -1,5 +1,5 @@
 // a series of closely spaced squares rotating clockwise along a sinusoidal path
-<Graph ylim={[-1.5, 1.5]} padding={0.2} aspect={phi}>
+<Graph ylim={[-1.5, 1.5]} padding={0.2} aspect={2}>
   <SymPoints
     fy={sin} xlim={[0, 2*pi]} size={0.5} N={150}
     shape={x => <Square rounded spin={r2d*x} />}
