6fda13fc477021871af48702b81ac2725dc76dba
Douglas Hanley <thesecretaryofwar@gmail.com>
2026-05-01 08:33:54 -0500

    update docs prompt

--- docs/code/TitleFrame.jsx
+++ docs/code/TitleFrame.jsx
@@ -1,4 +1,4 @@
-// Various food emojis are arrnaged in a spaced out grid and framed with the title "Fruits & Veggies". Each emoji is framed by a rounded square with a gray background.
+// Various food emojis are arrnaged in a spaced out grid and framed with the title "Fruits & Veggies". Each emoji is framed by a rounded square
 const emoji = [ '🍇', '🥦', '🍔', '🍉', '🍍', '🌽', '🍩', '🥝', '🍟' ]
 return <TitleFrame title="Fruits & Veggies" margin padding rounded>
   <Grid rows={3} spacing={0.05}>
