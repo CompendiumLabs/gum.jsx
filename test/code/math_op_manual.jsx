367c314a42c794f5ae3036527abab2fc930382f2
Douglas Hanley <thesecretaryofwar@gmail.com>
2026-08-18 11:50:04 -0500

    new math tests

---
+++ test/code/math_op_manual.jsx
@@ -0,0 +1,17 @@
+// MathOp handles operators by command, glyph, or name; display-style limits and side scripts follow automatically, and string scripts are parsed as TeX
+<MathText>
+  <SupSub sup="n" sub="i=0"><MathOp>\sum</MathOp></SupSub>
+  <SupSub sub="i"><MathSymbol>x</MathSymbol></SupSub>
+  <MathSymbol>+</MathSymbol>
+  <SupSub sup="1" sub="0"><MathOp>∫</MathOp></SupSub>
+  <MathSymbol>f</MathSymbol>
+  <MathSymbol>+</MathSymbol>
+  <SupSub sub="x \to 0"><MathOp>lim</MathOp></SupSub>
+  <MathSymbol>g</MathSymbol>
+  <MathSymbol>+</MathSymbol>
+  <SupSub sup="2"><MathOp>\sin</MathOp></SupSub>
+  <MathSymbol>x</MathSymbol>
+  <MathSymbol>+</MathSymbol>
+  <SupSub sup="n" sub="i=0"><MathOp style="text">\prod</MathOp></SupSub>
+  <SupSub sub="i"><MathSymbol>y</MathSymbol></SupSub>
+</MathText>
---
+++ test/code/math_string_leaves.jsx
@@ -0,0 +1,8 @@
+// string leaves in MathText, SupSub, and Frac are parsed as TeX in the appropriate style
+<MathText>
+  <SupSub sup="n+1" sub="i,j">x</SupSub>
+  <MathSymbol>=</MathSymbol>
+  <Frac>{["\\alpha^2 + 1", "\\sqrt{\\beta}"]}</Frac>
+  <MathSymbol>+</MathSymbol>
+  {"\\gamma_k \\cdot 3"}
+</MathText>
