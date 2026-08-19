7452aae5ea3b61545ac47b92a9ffc75a30ed9661
Douglas Hanley <thesecretaryofwar@gmail.com>
2026-08-19 11:10:24 -0500

    more testing

--- test/code/axis_arrows.jsx
+++ test/code/axis_arrows.jsx
@@ -1,8 +1,4 @@
-// axis arrowheads: a Plot with arrows on the right and top ends (styled per side and globally), plus standalone axes with arrows on both ends
-<HStack spacing={0.1}>
-  <Plot aspect={phi} margin={0.15} xlim={[0, 4]} ylim={[0, 1]} xticks={[0, 1, 2, 3]} yticks={[0, 0.25, 0.5, 0.75]} axis-arrow-right axis-arrow-top axis-arrow-fill={blue} yaxis-arrow-top-fill={red} xaxis-arrow-size={3}>
-    <SymLine fy={x => sqrt(x) / 2} xlim={[0, 4]} />
-  </Plot>
-  <Box padding={[0.5, 1]}><HAxis aspect={8} ticks={[0.25, 0.5, 0.75]} label-size={0.6} arrow-left arrow-right /></Box>
-  <Box padding={[1, 0.5]}><VAxis aspect={1/8} ticks={[0.25, 0.5, 0.75]} label-size={0.6} arrow-top arrow-bottom arrow-fill={blue} /></Box>
-</HStack>
+// axis arrowheads: a Plot of log(x) with arrows on the right and top ends
+<Plot aspect={phi} margin={0.15} xlim={[1, 5]} ylim={[0, 2]} xticks={range(1, 5)} yticks={linspace(0, 1.5, 4)} grid xaxis-arrow-right yaxis-arrow-top>
+  <SymLine fy={log} xlim={[1, 5]} />
+</Plot>
