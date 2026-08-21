41de290bfc8f5a36f52ae606318ade6502dad6be
Douglas Hanley <thesecretaryofwar@gmail.com>
2026-08-20 21:45:28 -0500

    remove test axis arrows

--- test/code/axis_arrows.jsx
+++
@@ -1,4 +0,0 @@
-// axis arrowheads: a Plot of log(x) with arrows on the right and top ends
-<Plot aspect={phi} margin={0.15} xlim={[1, 5]} ylim={[0, 2]} xticks={range(1, 5)} yticks={linspace(0, 1.5, 4)} grid xaxis-arrow-right yaxis-arrow-top>
-  <SymLine fy={log} xlim={[1, 5]} />
-</Plot>
